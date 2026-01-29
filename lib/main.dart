7ec2ef1317c92ac7071a104bcb6ef3b03f0aa3b4
arpitfaldu-pirimid <arpit.faldu@pirimidtech.com>
2026-01-29 11:10:29 +0530

    Added .env for the url's and configured to show proper errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
-
 import 'controller/stock_controller.dart';
 import 'screens/stock_screen.dart';
 
 Future<void> main() async {
 
-  // Required before async calls in main
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Load .env file
   await dotenv.load(fileName: ".env");
-
   runApp(const StockApp());
 }
 
@@ -21,21 +16,14 @@ class StockApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return ChangeNotifierProvider(
-
       create: (_) => StockController(),
-
       child: MaterialApp(
-
         debugShowCheckedModeBanner: false,
-
         title: 'Stock Live',
-
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-
         home: const StockScreen(),
       ),
     );
